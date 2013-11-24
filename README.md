4fe8a2963968190496f9e1f4f66f01579592afd0
Nick Herro <ncherro@gmail.com>
2013-11-24 01:07:23 -0500

    x

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Coming soon
 
 1. `brew install libspotify`
 1. `npm install`
+1. `export SPOTIFY_USERNAME="my-premium-account-username"`
+1. `export SPOTIFY_PASSWORD="my-premium-account-password"`
+1. `node app`
--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var app = require('express')(),
     server = require('http').createServer(app),
-    io = require('socket.io').listen(server);
+    io = require('socket.io').listen(server),
+    spotify = require('node-spotify')({
+      appkeyFile: __dirname + '/config/spotify_appkey.key'
+    });
 
 
 server.listen(3000);
@@ -11,7 +14,25 @@ app.get('/', function (req, res) {
 });
 
 
+
+spotify.ready( function() {
+  var playlists = spotify.getPlaylists();
+  console.log(playlists.length);
+  console.log(playlists[0].getTracks());
+  //spotify.player.play('spotify:track:5421318643512hd45');
+});
+spotify.login(
+  process.env.SPOTIFY_USERNAME,
+  process.env.SPOTIFY_PASSWORD,
+  false,
+  false
+);
+
+
+
+
 io.sockets.on('connection', function (socket) {
+  socket.emit
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
